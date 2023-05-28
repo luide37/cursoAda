4606d1178c9793c95b6745cce6241e20a9e7decf
jedi <lucasjhlcv@gmail.com>
2023-05-28 18:13:06 -0300

    arrays parte 2

---
+++ JavaScript/arrays_parte2.js
@@ -0,0 +1,51 @@
+// Arrays - parte 2 metodos
+
+const arr1 =[10, 40, 50, 80];
+const arr2 =[20, 30, 41, 98];
+
+// fatiamento slice
+
+// console.log(arr1.slice(0,2));
+// console.log(arr1.slice(2));
+
+// adicionando elementos push | unshit
+
+// arr1.push(88); //no final do array
+
+// arr1.unshift(0); //no começo do array
+
+
+// removendo elemntos  pop  shift 
+
+// arr1.pop()
+// arr1.shift()
+
+// conectando arrays   concat
+
+// console.log(arr1.concat(arr2));
+
+//  buscando elemntos: indexOf | lastIndexOf
+
+
+
+// let indiceElemento =  arr1.indexOf(50);
+// let indiceElemento2 =  arr1.indexOf(50);
+
+
+// console.log(indiceElemento);
+
+
+// descobrindo a existencia de um elemento  includes
+
+// console.log(arr1.includes(50));
+
+//invertendo arrays : reverse
+
+const arr1Invertido = arr1.reverse();
+
+
+console.log(arr1Invertido);
+console.log(arr2);
+
+
+
