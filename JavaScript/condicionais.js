0ab2bd79b3d819ab6b257fe87bf4ce52676385c6
jedi <lucasjhlcv@gmail.com>
2023-05-28 16:23:01 -0300

    condicionais

---
+++ JavaScript/condicionais.js
@@ -0,0 +1,29 @@
+//estruturas
+
+const idade = 10;
+
+if (idade >= 18) {
+    console.log('Você é maior de idade!');
+} else {
+    console.log('Você é menor de idade!');
+}
+
+console.clear();
+
+ let media = 10;
+
+if (media >= 7) {
+    console.log('Você esta APROVADO');
+} else if (media < 7 && media >= 5) {
+    console.log(media,'Você esta de RECUPERAÇÃO');
+} else {
+    console.log(media,'Você esta REPROVADO');
+}
+
+console.clear();
+
+var x = 10;
+
+console.log(x + y);
+
+let y = 15;
