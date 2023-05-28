4ff821df3b30ede8101597cc03bb9b8eab1ecc2e
jedi <lucasjhlcv@gmail.com>
2023-05-28 15:09:53 -0300

    operadores

---
+++ JavaScript/operadores_aritimeticos.js
@@ -0,0 +1,20 @@
+// boas praticas e operadores ariti
+
+let primeiroNumero = 20;
+let segundoNumero = 2;
+
+// adiçao
+// subtração
+// divisão
+// exponenciação
+// resto da divisão
+console.log('Soma =', primeiroNumero + segundoNumero)
+console.log('Soma =', primeiroNumero - segundoNumero)
+console.log('Soma =', primeiroNumero * segundoNumero)
+console.log('Soma =', primeiroNumero / segundoNumero)
+console.log('Soma =', primeiroNumero ** segundoNumero)
+console.log('Soma =', primeiroNumero % 3)
+console.log((3 + 7) * 3);
+console.log((10 + 7 + 8)/3);
+
+console.log(Math.PI);
